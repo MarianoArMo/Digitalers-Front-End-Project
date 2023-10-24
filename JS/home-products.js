f37b37f1aa44739266e7c184c6c918df861319e1
Mariano <marianoalejandroariasmorer@gmail.com>
2023-10-24 20:02:51 -0300

    Proyecto segunda entrega

--- JS/home-products.js
+++
@@ -1,6 +0,0 @@
-const mainProducts = JSON.parse(localStorage.getItem('products')) || albums
-
-if ( !localStorage.getItem("products") ) {
-    localStorage.setItem("products", JSON.stringify(mainProducts))
-}
-
